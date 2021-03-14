20ffacb72de9f0c276feb4b8b8868c8995e10fe3
Shyaboi <ian.shane.sears@gmail.com>
2021-03-13 22:15:20 -0700

    faqs ready for 1st deploy

--- src/Pages/FAQs/FAQs.jsx
+++ src/Pages/FAQs/FAQs.jsx
@@ -4,6 +4,7 @@ import {
   CardText,
   CardBody,
   CardTitle,
+  CardHeader,
   CardSubtitle,
   Button,
   Dropdown,
@@ -20,9 +21,9 @@ import { Container, Row, Col } from "reactstrap";
 import fetchy from "../../Utils/Fetcher";
 
 const FAQs = () => {
-  const [ff, setFAQs] = useState();
+  const [ff, setFAQs] = useState([]);
 
-  const ok = () => {
+  const ok = (props) => {
     fetchy("https://openflags.net/faqs").then(async (data) => {
       console.log(data);
       let d = data.faq;
@@ -37,24 +38,87 @@ const FAQs = () => {
   }, []);
 
   return (
-    <Container>
+    <Container fluid>
       <Row className="mt-5 text-center">
         <Col className="mt-5">
-          <h1>FAQs</h1>
+          <Row>
+            <Col>
+              <h1>Ask a questiong to the Open Flags API Team</h1>
+            </Col>
+          </Row>
+      <Container>
+          <Form method="POST" action="https://openflags.net/newfaq">
+            <Row xs="1" md="2">
+              <Col>
+                <FormGroup >
+                  <Label for="exampleFormControlSelect2">Are You a...</Label>
+                  <Input
+                    type="select"
+                    className="form-control"
+                    id="exampleFormControlSelect2"
+                    name="person"
+                  >
+                    <option>Developer</option>
+                    <option>Flag Enthusiest</option>
+                    <option>History Enthusiest</option>
+                    <option>Crazy Person</option>
+                    <option>Normal Person</option>
+                  </Input>
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup >
+                  <Label for="inputState">What kind of feedback are you providing?</Label>
+                  <Input
+                    type="select"
+                    id="inputState"
+                    class="form-control"
+                    name="type"
+                    >
+                    <option value="Question">Question</option>
+                    <option value="Suggestion">Suggestion</option>
+                    <option value="Comment">Comment</option>
+                    <option value="Comment">Criticism</option>
+                  </Input>
+                  </FormGroup>
+                    </Col>
+                    
+                    </Row>
+                  <Row>
+                    <Col>
+                  <FormGroup >
+                    <Label for="exampleFormControlTextarea1">Message</Label>
+                    <Input
+                      type="textarea"
+                      class="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                      name="message"
+                    ></Input>
+                  </FormGroup>
 
+                  <Button type="submit" className="btn btn-other">
+                    Submit
+                  </Button>
+                  </Col>
+                </Row>
+          </Form>
+          </Container>
           <hr />
         </Col>
       </Row>
       <Container>
-        <Row md="2">
+        <Row xs="1" xl="2">
           {ff.map((fl) => {
             return (
-              <Row>
+              <Row className="p-2 mt-5 text-center">
                 <Col>
                   <Card body className="text-center">
                     <Row>
                       <Col>
-                        <CardTitle tag="h5"> {fl.person}</CardTitle>
+                        <CardHeader>{fl.person}</CardHeader>
+
+                        <CardTitle tag="h5"> </CardTitle>
                         <hr />
                       </Col>
                     </Row>
@@ -65,13 +129,13 @@ const FAQs = () => {
                       </Col>
                     </Row>
                     {/* <Row>
-                    <Col>
-                    <CardText>{fl.vote}</CardText>
-                    </Col>
-                  </Row> */}
+                      <Col>
+                        <CardText>{fl.vote}</CardText>
+                      </Col>
+                    </Row> */}
                     <Row>
                       <Col>
-                        <p> Reply from OF Team: </p>
+                        <p> Reply from Open Flags Team: </p>
                         {fl.reply}
                       </Col>
                     </Row>
